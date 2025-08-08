ad6346ffa5b2e828e9812b7a5af6a795bf3c4c7d
Rose <Rose@DESKTOP-G9LQFUB>
2025-08-08 19:47:05 +0100

    Implemented Basic Listener

---
+++ src/main/java/me/rosariagx/ethernetCore/Listeners/PlayJoinEventListener.java
@@ -0,0 +1,27 @@
+package me.rosariagx.ethernetCore.Listeners;
+
+import me.rosariagx.ethernetCore.Main;
+import net.kyori.adventure.Adventure;
+import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayJoinEventListener implements Listener {
+
+    private Main plugin;
+
+    public PlayJoinEventListener(Main plugin) {
+        this.plugin = plugin;
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler
+    private void PlayerJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+
+        player.sendMessage(Component.text("Welcome to The Official Ethernet Test Deployment!!"));
+    }
+
+}
--- src/main/java/me/rosariagx/ethernetCore/Main.java
+++ src/main/java/me/rosariagx/ethernetCore/Main.java
@@ -1,17 +1,18 @@
 package me.rosariagx.ethernetCore;
 
+import me.rosariagx.ethernetCore.Listeners.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
+        new PlayJoinEventListener(this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
